d38ddf40c428129439abee9494ed4a597394f596
Goorer <at4230392@gmail.com>
2022-07-02 18:05:14 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const todoEle = document.getElementById('todo');
+const addEle = document.getElementById('add');
+const listEle = document.getElementById('list');
+let items = [];
+
+const addItem = () => {
+    if(todoEle.value != ''){
+        items.push(todoEle.value);
+        console.log(items);
+        todoEle.value='';
+        createList();
+    }
+}
+
+function createList() {
+    if(listEle.childElementCount > 0){
+        while(listEle.firstChild){
+            listEle.removeChild(listEle.firstChild);
+        }
+    }
+
+    const tr1 = document.createElement('tr');
+    const th1 = document.createElement('th');
+    const th2 = document.createElement('th');
+    const th3 = document.createElement('th');
+    th1.innerText = 'No.';
+    th2.innerText = 'Todo';
+    th3.innerText = 'Ok';
+    tr1.appendChild(th1);
+    tr1.appendChild(th2);
+    tr1.appendChild(th3);
+    listEle.appendChild(tr1);
+    
+    items.forEach((item,index) => {
+        const tr2 = document.createElement('tr');
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        const chk = document.createElement('input');
+        td1.innerText = index+1;
+        td2.innerText = item;
+        chk.type = 'checkbox';
+        chk.id = index;
+        chk.name = 
+        chk.addEventListener('change',() =>{
+            const chkEle = document.getElementsByName
+            if(chk.checked){
+                items.splice(chk.id,1);
+                createElement();
+            }
+        });
+        tr2.appendChild(td1);
+        tr2.appendChild(td2);
+        tr2.appendChild(chk);
+        listEle.appendChild(tr2);
+        console.log('createList');
+    });
+    
+}
+
+addEle.addEventListener('click',addItem);
